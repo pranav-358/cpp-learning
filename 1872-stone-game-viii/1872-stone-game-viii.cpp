2c7e1856257b60fda36ab23bc8140bf23847e03c
Pranav Tayade <pranavtayade30@gmail.com>
2026-08-24 20:44:25 +0530

    Time: 92 ms (93.85%), Space: 87.2 MB (84.19%) - LeetHub

--- 1872-stone-game-viii/1872-stone-game-viii.cpp
+++ 1872-stone-game-viii/1872-stone-game-viii.cpp
@@ -5,9 +5,7 @@ public:
         for (int i = 1; i < n; ++i) {
             stones[i] += stones[i - 1];
         }
-    
         int dp = stones[n - 1];
-
         for (int i = n - 2; i >= 1; --i) {
             dp = max(dp, stones[i] - dp);
         }
