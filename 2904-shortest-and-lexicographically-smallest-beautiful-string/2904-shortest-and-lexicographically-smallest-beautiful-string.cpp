f63f8a77d18846bd2671244b5969058da644b5f9
Pranav Tayade <pranavtayade30@gmail.com>
2026-08-26 20:32:50 +0530

    Time: 4 ms (19.14%), Space: 8.6 MB (82.87%) - LeetHub

--- 2904-shortest-and-lexicographically-smallest-beautiful-string/2904-shortest-and-lexicographically-smallest-beautiful-string.cpp
+++ 2904-shortest-and-lexicographically-smallest-beautiful-string/2904-shortest-and-lexicographically-smallest-beautiful-string.cpp
@@ -5,16 +5,19 @@ public:
         int minLen = INT_MAX;
         for (int i = 0; i < s.size(); i++) {
             int ones = 0;
-            for (int j = i; j < s.size(); j++) {
+            for (int j = i; j < s.size(); j++)
+             {
                 if (s[j] == '1')
                     ones++;
-                if (ones == k) {
+                if (ones == k)
+                 {
                     string sub = s.substr(i, j - i + 1);
                     if (sub.length() < minLen) {
                         minLen = sub.length();
                         ans = sub;
                     }
-                    else if (sub.length() == minLen && sub < ans) {
+                    else if (sub.length() == minLen && sub < ans)
+                     {
                         ans = sub;
                     }
                     break;
